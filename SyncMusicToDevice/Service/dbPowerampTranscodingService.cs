cef8d86a497c40b09e2732ee8344d31c28784e38
Ben Hutchison <ben@aldaviva.com>
2018-12-07 04:41:25 -0800

    Increase transcoded AAC bitrate from VBR quality 4 (128 kbps) to 5 (192 kbps)

--- SyncMusicToDevice/Service/dbPowerampTranscodingService.cs
+++ SyncMusicToDevice/Service/dbPowerampTranscodingService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using NLog;
 using SyncMusicToDevice.Injection;
@@ -9,6 +7,12 @@ namespace SyncMusicToDevice.Service
 {
     /**
      * COM add-in seems to be single-threaded, but forking processes is parallelizable
+     *
+     * fdkaac arguments: https://wiki.hydrogenaud.io/index.php?title=Fraunhofer_FDK_AAC#fdkaac
+     * To see arguments sent by dBpoweramp UI, use Process Monitor to trace Process Start event on fdkaac.exe
+     *
+     * -m 5 means use the highest VBR quality, Quality 5 (estimated bit rate: 224 kbps) [although it seems to actually be more like 192 kbps]
+     * -p 2 means use AAC-LC (Low Complexity) instead of HE-AAC or HE-AACv2 which are for lower bitrates
      */
     [Component]
     // ReSharper disable once InconsistentNaming
@@ -19,7 +23,7 @@ namespace SyncMusicToDevice.Service
         private static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
 
         private const string EncoderName = @"m4a FDK (AAC)";
-        private const string CompressionSettings = @"-cli_cmd=""-m 4 -p 2 --ignorelength -S -o {qt}[outfile]{qt} - """;
+        private const string CompressionSettings = @"-cli_cmd=""-m 5 -p 2 --ignorelength -S -o {qt}[outfile]{qt} - """;
         private const string CoreConverterPath = @"c:\Programs\Multimedia\dBpoweramp\CoreConverter.exe";
 
         public Task Transcode(string sourceFile, string destinationFile)
